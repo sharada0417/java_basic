9f3d01e901c9c236a9cfff8597a3703a22be8572
sharada417 <sharada.kanchana2001@gmail.com>
2025-03-15 12:21:17 +0530

    initial commit

---
+++ Four.java
@@ -0,0 +1,10 @@
+//print the odd numbers between 1 to 20
+class Four{
+    public static void main(String[] args) {
+        for(int i=1; i<=20; i++){
+            if(i%2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ Third.java
@@ -0,0 +1,7 @@
+class Third{
+	public static void main(String [] args){
+		for(int i=10; i>1; i--){
+			System.out.println(i);
+		}
+	}
+}
